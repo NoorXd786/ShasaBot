14a1a71081ea30615c68a60ce50cb4c6012648a5
Mdnoor786 <75603153+Mdnoor786@users.noreply.github.com>
2021-05-13 08:10:40 +0530

    Update misc.py

--- ShasaBot/modules/helper_funcs/misc.py
+++ ShasaBot/modules/helper_funcs/misc.py
@@ -122,6 +122,17 @@ def sendMessage(text: str, bot: Bot, update: Update):
                                     text=text, parse_mode=ParseMode.HTML)
 
 
+def build_keyboard_parser(bot, chat_id, buttons):
+    keyb = []
+    for btn in buttons:
+        if btn.url == "{rules}":
+            btn.url = "http://t.me/{}?start={}".format(bot.username, chat_id)
+        if btn.same_line and keyb:
+            keyb[-1].append(InlineKeyboardButton(btn.name, url=btn.url))
+        else:
+            keyb.append([InlineKeyboardButton(btn.name, url=btn.url)])
+
+    return keyb
 
 def is_module_loaded(name):
      return name not in NO_LOAD

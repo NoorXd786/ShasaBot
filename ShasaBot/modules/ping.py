3996f572df102870960651b08fdc21510ae34573
Mdnoor786 <75603153+Mdnoor786@users.noreply.github.com>
2021-05-13 12:54:27 +0530

    Update ping.py

--- ShasaBot/modules/ping.py
+++ ShasaBot/modules/ping.py
@@ -74,15 +74,15 @@ def ping(update: Update, context: CallbackContext):
     msg = update.effective_message
 
     start_time = time.time()
-    message = msg.reply_text("Pinging...")
+    message = msg.reply_text("𝕻𝖎𝖓𝖌𝖎𝖓𝖌...")
     end_time = time.time()
     telegram_ping = str(round((end_time - start_time) * 1000, 3)) + " ms"
     uptime = get_readable_time((time.time() - StartTime))
 
     message.edit_text(
-        "PONG!!\n"
-        "<b>Time Taken:</b> <code>{}</code>\n"
-        "<b>Service uptime:</b> <code>{}</code>".format(telegram_ping, uptime),
+        "𝕻𝖔𝖓𝖌!!\n"
+        "<b>𝕿𝖎𝖒𝖊 𝖙𝖆𝖐𝖊𝖓:</b> <code>{}</code>\n"
+        "<b>𝕾𝖊𝖗𝖛𝖎𝖈𝖊 𝖚𝖕𝖙𝖎𝖒𝖊:</b> <code>{}</code>".format(telegram_ping, uptime),
         parse_mode=ParseMode.HTML,
     )
 
@@ -97,7 +97,7 @@ def pingall(update: Update, context: CallbackContext):
 
     reply_msg = "⏱Ping results are:\n"
     reply_msg += "\n".join(pinged_list)
-    reply_msg += "\n<b>Service uptime:</b> <code>{}</code>".format(uptime)
+    reply_msg += "\n<b>𝕾𝖊𝖗𝖛𝖎𝖈𝖊 𝖚𝖕𝖙𝖎𝖒𝖊:</b> <code>{}</code>".format(uptime)
 
     update.effective_message.reply_text(
         reply_msg, parse_mode=ParseMode.HTML, disable_web_page_preview=True

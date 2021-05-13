8c39c9e12c5c3bae1d95468896cf597987642fd9
Mdnoor786 <75603153+Mdnoor786@users.noreply.github.com>
2021-05-13 07:57:36 +0530

    Update misc.py

--- ShasaBot/modules/helper_funcs/misc.py
+++ ShasaBot/modules/helper_funcs/misc.py
@@ -69,7 +69,7 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
     if len(pairs) > 7:
         pairs = pairs[modulo_page * 10:10 * (modulo_page + 1)] + [
             (EqInlineKeyboardButton("👈🏻", callback_data="{}_prev({})".format(prefix, modulo_page)),
-                EqInlineKeyboardButton("⏹🌹, callback_data="bot_start"),
+                EqInlineKeyboardButton("🌹, callback_data="bot_start"),
              EqInlineKeyboardButton("👉🏻", callback_data="{}_next({})".format(prefix, modulo_page)))]
 
     else:

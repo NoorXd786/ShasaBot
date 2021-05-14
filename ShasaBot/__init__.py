71c192761b1a3bd22c3eb7fc10263177ae74c9c9
Aquila-14 <78516399+Aquila-14@users.noreply.github.com>
2021-05-14 07:07:18 +0530

    Update __init__.py

--- ShasaBot/__init__.py
+++ ShasaBot/__init__.py
@@ -82,6 +82,7 @@ if ENV:
     NO_LOAD = os.environ.get("NO_LOAD", "translation").split()
     DEL_CMDS = bool(os.environ.get("DEL_CMDS", False))
     STRICT_GBAN = bool(os.environ.get("STRICT_GBAN", False))
+    STRICT_GMUTE = bool(os.environ.get('STRICT_GMUTE', True))
     WORKERS = int(os.environ.get("WORKERS", 8))
     BAN_STICKER = os.environ.get("BAN_STICKER", "CAADAgADOwADPPEcAXkko5EB3YGYAg")
     ALLOW_EXCL = os.environ.get("ALLOW_EXCL", False)

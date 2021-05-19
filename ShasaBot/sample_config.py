9e5bb3a951deb119bc4a8dc85391d96197084ada
Mdnoor786 <75603153+Mdnoor786@users.noreply.github.com>
2021-05-20 05:28:23 +0530

    Update sample_config.py

--- ShasaBot/sample_config.py
+++ ShasaBot/sample_config.py
@@ -17,7 +17,7 @@ class Config(object):
     API_ID = 123456  # integer value, dont use ""
     API_HASH = "awoo"
     TOKEN = "BOT_TOKEN"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 1099219137  # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    OWNER_ID = 1837687523  # If you dont know, run the bot and do /id in your private chat with it, also an integer
     OWNER_USERNAME = "MdNoor"
     SUPPORT_CHAT = "MissShasa_Gbot"  # Your own group for support, do not add the @
     JOIN_LOGGER = (
